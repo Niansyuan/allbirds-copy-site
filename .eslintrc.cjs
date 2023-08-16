b18c4f0bf48246e87c29defe1a8d1df8b10d7f5a
Michelle Wang <michelle.wang@onelab.tw>
2023-08-16 20:29:42 +0800

    Feat: basic lint setting

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'airbnb-base',
+    'prettier',
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.js', '.vue'],
+      },
+    },
+    'import/core-modules': ['vite', '@vitejs/plugin-vue'],
+  },
+};
