730d2ea1438fbf30f2aedfd369ee5f2d13160754
Michelle Wang <michelle.wang@onelab.tw>
2023-09-13 19:51:48 +0800

    Feat: Daniel切版示範

---
+++ src/components/BasicCard.vue
@@ -0,0 +1,28 @@
+<script setup>
+import ButtonComponent from '@/components/ButtonComponent.vue';
+</script>
+
+<template>
+    <div class="h-80 w-64 bg-slate-50">
+        <div class="flex flex-col items-center gap-2">
+            <div class="h-8 w-2/3">
+                <ButtonComponent
+                    button-text="BUTTON A"
+                    button-color="primary"
+                    button-border="primary"
+                />
+            </div>
+            <div class="h-8 w-2/3">
+                <ButtonComponent
+                    button-text="BUTTON B"
+                    button-color="primary"
+                    button-border="primary"
+                />
+            </div>
+        </div>
+        <div class="px-7 py-4">
+            <div>wool</div>
+            <div>cozy</div>
+        </div>
+    </div>
+</template>
--- src/components/CataLogueComponent.vue
+++ src/components/CataLogueComponent.vue
@@ -1,13 +1,20 @@
 <script setup>
-import ButtonComponent from '@/components/ButtonComponent.vue';
+import BasicCard from '@/components/BasicCard.vue';
 </script>
 
 <template>
-    <div
-        class="box-border flex h-full w-full flex-col items-center justify-center bg-gray-200 py-20"
-    >
-        <div class="text-lg font-bold text-gray-800">TITLE OF CATALOGUE</div>
-        <!-- TABS component -->
+    <div class="flex flex-col items-center py-10">
+        <div class="py-4">Our Favorites</div>
+        <div class="bg-slate-100">
+            <div class="mb-2 flex items-center justify-center">tab tab tab</div>
+            <div class="flex items-center justify-between gap-4">
+                <BasicCard></BasicCard>
+                <BasicCard></BasicCard>
+                <BasicCard></BasicCard>
+            </div>
+        </div>
+        <!-- <div class="text-lg font-bold text-gray-800">TITLE OF CATALOGUE</div>
+        TABS component
         <div
             class="flex w-10/12 items-center justify-center gap-10 border-b-2 border-gray-400 pt-5"
         >
@@ -15,31 +22,9 @@ import ButtonComponent from '@/components/ButtonComponent.vue';
             <div class="text-gray-400 hover:text-gray-700">TAB B</div>
             <div class="text-gray-400 hover:text-gray-700">TAB C</div>
         </div>
-        <!-- CARDS component -->
+        CARDS component
         <div class="py-5">
-            <!-- CARD component -->
-            <div class="h-80 w-64 bg-slate-50">
-                <div class="flex flex-col items-center gap-2">
-                    <div class="h-8 w-2/3">
-                        <ButtonComponent
-                            button-text="BUTTON A"
-                            button-color="primary"
-                            button-border="primary"
-                        />
-                    </div>
-                    <div class="h-8 w-2/3">
-                        <ButtonComponent
-                            button-text="BUTTON B"
-                            button-color="primary"
-                            button-border="primary"
-                        />
-                    </div>
-                </div>
-                <div class="px-7 py-4">
-                    <div>wool</div>
-                    <div>cozy</div>
-                </div>
-            </div>
-        </div>
+            CARD component
+        </div> -->
     </div>
 </template>
