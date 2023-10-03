4fb234ff9412656d9d450a5af64530e5e3428e3e
Michelle Wang <michelle.wang@onelab.tw>
2023-10-03 21:01:30 +0800

    Feat: 安裝storybook
    - FIXME: script 指令有誤帶解

---
+++ .storybook/preview.js
@@ -0,0 +1,9 @@
+import { setup } from '@storybook/vue3';
+
+setup((app) => {
+    app.use(MyPlugin);
+    app.component('my-component', MyComponent);
+    app.mixin({
+        /* My mixin */
+    });
+});
