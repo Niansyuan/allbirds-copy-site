674c32db114e53bbef98de317a6a9ae816a7b65e
Michelle Wang <michelle.wang@onelab.tw>
2023-10-22 17:03:33 +0800

    Feat: emit 練習

--- src/components/composite/LayoutBanner.vue
+++ src/components/composite/LayoutBanner.vue
@@ -1,13 +1,22 @@
 <script setup>
+import { defineEmits, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 import BasicButton from '@/components/view/BasicButton.vue';
+import LayoutPopup from './LayoutPopup.vue';
 
 const button = 'button bind'; // 常數宣告，若要使用常數，就需要再下方使用bind :button="button"
 const router = useRouter();
+const isShowPopupBanner = ref(false);
+const { togglePopup } = defineEmits(['update:is-Show']);
 
 const clickBannerLink = path => {
     router.push(path);
+};
+
+const clickBannerPopup = () => {
+    isShowPopupBanner.value = !isShowPopupBanner.value;
+    togglePopup(isShowPopupBanner.value);
 };
 </script>
 
@@ -15,6 +24,12 @@ const clickBannerLink = path => {
     <div
         class="relative flex h-full w-full flex-col items-center justify-center bg-[url('/banner.jpg')] bg-cover bg-no-repeat"
     >
+        <div class="relative h-max w-full">
+            <LayoutPopup
+                :is-show="isShowPopupBanner"
+                @update:is-show="togglePopup"
+            />
+        </div>
         <div class="text-2xl font-bold text-white">I AM BANNER</div>
         <div class="absolute bottom-10 flex gap-4">
             <div class="flex h-10 w-40">
@@ -27,9 +42,10 @@ const clickBannerLink = path => {
             </div>
             <div class="flex h-10 w-40">
                 <BasicButton
-                    button-text="button"
+                    button-text="show"
                     button-color="secondary"
                     button-border="primary"
+                    :button-action="clickBannerPopup"
                 />
             </div>
         </div>
---
+++ src/components/composite/LayoutPopup.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { computed, defineProps } from 'vue';
+
+import BasicButton from '@/components/view/BasicButton.vue';
+
+const props = defineProps(['isShow']);
+
+const displayPopup = computed(() => {
+    console.log(props.isShow);
+    return props.isShow;
+});
+</script>
+<template>
+    <div
+        v-if="displayPopup"
+        class="left-2 top-2 flex h-80 w-64 shrink-0 flex-col items-center bg-slate-50"
+    >
+        <div>Title Popup</div>
+        <div>Popup Content</div>
+        <div class="flex h-10 w-40">
+            <BasicButton
+                button-text="Close"
+                button-color="secondary"
+                button-border="primary"
+            />
+        </div>
+    </div>
+</template>
